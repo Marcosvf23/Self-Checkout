3da75511c9ce8441de09d278fc7c5ad444457072
Marcos Fernandes <marcosvf23@hotmail.com>
2025-11-28 14:51:29 -0300

    feat: foi adicionado tela de visualizar pedidos

---
+++ prisma/migrations/20251128172817_add_customer_to_order/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - The values [COMPLETED] on the enum `OrderStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "OrderStatus_new" AS ENUM ('PENDING', 'IN_PREPARATION', 'FINISHED');
+ALTER TABLE "Order" ALTER COLUMN "status" TYPE "OrderStatus_new" USING ("status"::text::"OrderStatus_new");
+ALTER TYPE "OrderStatus" RENAME TO "OrderStatus_old";
+ALTER TYPE "OrderStatus_new" RENAME TO "OrderStatus";
+DROP TYPE "OrderStatus_old";
+COMMIT;
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_productId_fkey";
+
+-- DropIndex
+DROP INDEX "Restaurant_slug_key";
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20251128173816_add_customer_to_order2/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[slug]` on the table `Restaurant` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `customerCpf` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `customerName` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "OrderStatus" ADD VALUE 'PAYMENT_CONFIRMED';
+ALTER TYPE "OrderStatus" ADD VALUE 'PAYMENT_FAILED';
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "customerCpf" TEXT NOT NULL,
+ADD COLUMN     "customerName" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_slug_key" ON "Restaurant"("slug");
